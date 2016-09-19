7dad87bd2e101a50cd77478852a79b16a4414c4b
BatBrain <john.ferguson3.141596@gmail.com>
2016-09-19 03:33:03 +0000

    add complete project

--- week2day3/express_server.js
+++ week2day3/express_server.js
@@ -1,13 +1,13 @@
 "use strict";
 
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded());
 app.set("view engine", "ejs");
 
-var urlDatabase = {
+let urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
--- week2day4/express_server.js
+++ week2day4/express_server.js
@@ -9,21 +9,17 @@ const connect = require('connect')
 const methodOverride = require('method-override')
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://127.0.0.1:27017/url_shortener";
-    //
+//
 
 //Set and Use Declarations:
 app.use(bodyParser.urlencoded());
+app.use(express.static("pictures"));
 app.set("view engine", "ejs");
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 //
 
-/*var urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-};
-let templateVars = { urls: urlDatabase };
-*/
-let db = null;
+
+var db = null;
 
 MongoClient.connect(MONGODB_URI, (err, database) => {
   if (err) {
@@ -38,10 +34,21 @@ MongoClient.connect(MONGODB_URI, (err, database) => {
 function getLongURL(db, shortURL, cb) {
   let query = { "shortURL": shortURL };
   db.collection("urls").findOne(query, (err, result) => {
-    if (err) {
+    if (result === null) {
+      console.log("Got nothin, mate!")
       return cb(err);
     }
     return cb(null, result.longURL);
+  });
+}
+
+function getShortURL(db, shortURL, cb) {
+  let query = { "shortURL": shortURL };
+  db.collection("urls").findOne(query, (err, result) => {
+    if (err) {
+      return cb(err);
+    }
+    return cb(null, result.shortURL);
   });
 }
 
@@ -68,9 +75,10 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
     var prefix = '';
     if ((`${req.body.longURL}`).search('http://')) {
-        var prefix = "http://" };
+        prefix = "http://"
+    }
     //urlDatabase[generateRandomString()] = `${prefix}${req.body.longURL}`;
-    db.collection("urls").insertOne({shortURL: [generateRandomString()], longURL: `${prefix}${req.body.longURL}`});
+    db.collection("urls").insertOne({shortURL: generateRandomString(), longURL: `${prefix}${req.body.longURL}`});
     db.collection("urls").find().toArray((err, data) => {
         res.render("urls_index", {urls: data});
     });
@@ -86,43 +94,57 @@ app.post("/urls", (req, res) => {
   });
 });*/
 
-
 // Checks if shortURL exists, redirects to longURL if true, ../urls if not (insert err notification here later)
 app.get("/urls/:id", (req, res) => {
-    if (db.collection("urls").find({urls: `${req.params.id}`})) {
-        //res.redirect(`${urlDatabase[req.params.id]}`)
-        let shortURL = req.params.id;
-        getLongURL(db, shortURL, (err, longURL) => {
-            console.log(longURL);
-            res.redirect(`${longURL}`);
-    });
-    }
-    db.collection("urls").find().toArray((err, data) => {
-        res.render("urls_index", {urls: data});
+    let query = {"shortURL": req.params.id};
+    db.collection("urls").findOne(query, (err, result) => {
+        if (err) {
+        console.log("Got nothin, mate!")
+        res.render("not_found", {shortURL: `${req.params.id}`})
+        return;
+        } else {
+        res.redirect(`${result.longURL}`);
+        }
     });
 });
 
 // delete button from ../urls removes from db then refreshes page
 app.delete("/urls/:id", (req, res) => {
-    console.log(`A user has deleted: [${req.params.id}: ${urlDatabase[req.params.id]}]`)
-    delete urlDatabase[`${req.params.id}`]
-    res.redirect("/urls");
+    let query = {"shortURL": req.params.id};
+    db.collection("urls").findOne(query, (err, result) => {
+        if (err) {
+        console.log("Just tried to delete nothing!")
+        res.redirect("/urls");
+        return;
+        } else {
+        db.collection("urls").remove(query, 1);
+        res.redirect("/urls");
+        }
+    })
 })
 
 // edit button from ../urls sends to edit page of specific pair.
 app.get("/urls/show/:id", (req, res) => {
-    let passedIn = {shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
-    res.render("urls_show", passedIn);
-})
+    let query = ({"shortURL": req.params.id});
+    db.collection("urls").findOne(query, (err, result) => {
+    if (err) {
+        res.redirect("/urls")
+        return;
+    } else {
+        let passIn = {shortURL: `${result.shortURL}`, longURL: `${result.longURL}`}
+        res.render("urls_show", passIn);
+        }
+    });
+});
 
 // submit req from ../urls/show/:id, updates short/long pair, redirects to ../urls
 app.put("/urls/:id", (req, res) => {
     var prefix = '';
     if ((`${req.body.longURL}`).search('http://')) {
-        var prefix = "http://" };
-    urlDatabase[req.params.id] = `${prefix}${req.body.longURL}`;
+        prefix = "http://" };
+    db.collection("urls").update({"shortURL": req.params.id}, {$set: {"longURL": `${prefix}${req.body.longURL}`}})
     res.redirect("/urls");
-})
+    })
 
 
 
@@ -133,17 +155,14 @@ app.listen(PORT, () => {
 
 function generateRandomString() {
     return Math.random().toString(36).slice(2, 8)
-};
+}
 
 
 
-//Catch process exits and close mongo connections
-/*process.on("SIGINT", () => {
+//Catch sigint and close mongo connections
+process.on("SIGINT", () => {
     console.log("Closing mongodb connection...");
     db.close();
+    console.log("Good bye.");
+    process.exit();
 });
-
-process.on("EXIT", () => {
-    console.log("Closing mongodb connection...");
-    db.close();
-});*/
